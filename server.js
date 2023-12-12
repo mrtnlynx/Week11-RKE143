7478f08fe6742d92a443b71776f5ce6b082e4f21
Martin Lynx <martilve@tktk.ee>
2023-12-12 21:13:46 +0200

    week 12

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const fullRecipesRouter = require('./routes/fullRecipes.routes');
 const randomRouter = require('./routes/randomRecipe.routes');
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
